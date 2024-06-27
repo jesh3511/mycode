5ce58f2d0e65eb07be41169e5d7e85a05950e3d1
jesh3511 <jeshua.gonzalez@tlgcohort.com>
2024-06-27 20:39:49 +0000

    mixed lists

---
+++ challenges/tuesdaychallenge.py
@@ -0,0 +1,5 @@
+#!/usr/bin python3
+
+mylist= [1,2,3,4,5, "Python"]
+name= input("What is your name?\n")
+print("Hi ", name.upper(),"! ", "Welcome to day ", mylist[2], " of ", mylist[5], " training!", sep='')
