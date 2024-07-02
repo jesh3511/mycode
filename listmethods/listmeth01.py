3d3e24bfb8d9f66ff191c10baf7861d5334c8bd6
jesh <amazing.jesh@gmail.com>
2024-07-02 15:47:26 +0000

    updated lab 23

--- listmethods/listmeth01.py
+++ listmethods/listmeth01.py
@@ -10,3 +10,8 @@ proto.extend(proto2) # pass proto2 as an argument to the extend method
 print(proto)
 protoa.append(proto2) # pass proto2 as an argument to the append method
 print(protoa)
+proto3=[1,2,3,4,5]#defining the list that will be used 
+proto.insert(2,proto3)#inserts the new list into proto list
+print(proto)#displays updated proto list
+protoa.insert(len(protoa),proto3)#equal to append
+print(protoa)#prints updated protoa list
