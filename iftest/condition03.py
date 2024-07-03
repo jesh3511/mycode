224814b39f671981b35df34a5e13b5cc2d072967
jesh <amazing.jesh@gmail.com>
2024-07-03 22:01:34 +0000

    if tests

--- iftest/condition03.py
+++ iftest/condition03.py
@@ -3,10 +3,10 @@
 # Check hostname against expected value
 
 ## Collect input from user
-hostname = input("What value should we set for hostname?")
+hostname = input("What value should we set for hostname?").lower()
 
-## use the lower method to test if input value matches expected value
-if hostname.lower() == "mtg":
+#use the lower method to test if input value matches expected value
+if hostname == "mtg":
     print("The hostname was found to be mtg")
     print("hostname matches expected config")
 
